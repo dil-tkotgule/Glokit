3353d1337188d8a5ce3b7785dbc219170850bf99
dil-pjangid <pjangid@diligent.com>
2025-07-21 11:39:44 +0530

    Sidebar render on user role

--- UI/src/component/SidePannel.tsx
+++ UI/src/component/SidePannel.tsx
@@ -1,14 +1,14 @@
-// SidePannel.tsx
 import React, { useState } from 'react';
 import { Box, CssBaseline, useTheme, useMediaQuery } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { type RootState } from '../redux/store'; // adjust path
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 interface SidePannelProps {
   children: React.ReactNode;
 }
-
 const SidePannel: React.FC<SidePannelProps> = ({ children }) => {
   const theme = useTheme();
   const location = useLocation();
@@ -18,11 +18,13 @@ const SidePannel: React.FC<SidePannelProps> = ({ children }) => {
   const [mobileOpen, setMobileOpen] = useState(false);
   const isLoginPage = location.pathname === '/login';
 
+  const user = useSelector((state: RootState) => state);
+  const role = user?.role || JSON.parse(localStorage.getItem('user') || '{}')?.role || '';
+  let probe = localStorage.getItem('userm') || '';
+
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
-  let probe = localStorage.getItem('userm') || '';
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -38,11 +40,15 @@ const SidePannel: React.FC<SidePannelProps> = ({ children }) => {
       )}
 
       {!isLoginPage && (
-        <Sidebar
-          variant={isSmallScreen ? 'temporary' : 'permanent'}
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-        />
+        role === 'fresher' ? (
+          <></>
+        ) : (
+          <Sidebar
+            variant={isSmallScreen ? 'temporary' : 'permanent'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+          />
+        )
       )}
 
       <Box
